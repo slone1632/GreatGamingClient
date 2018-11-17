8e7840866a7e1a4f06eb6796184098d00868b0e7
slone1632 <slone1632@gmail.com>
2018-11-17 16:06:17 -0500

    make client connect on one port and use another thereafter

--- greatgaming-client/src/main/java/com/greatgaming/ClientMain.java
+++ greatgaming-client/src/main/java/com/greatgaming/ClientMain.java
@@ -4,13 +4,31 @@ import java.io.*;
 import java.net.*;
 
 public class ClientMain {
+	private static Integer WELCOME_PORT = 6789;
+	
+	private static Integer getPort() throws Exception {
+		String persistentPort;
+		Socket clientSocket = new Socket("localhost", WELCOME_PORT);
+		
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+		outToServer.writeBytes("RequestingNewConnection" + System.lineSeparator());
+		persistentPort = inFromServer.readLine();
+		System.out.println("Server wants to establish connection on port " + persistentPort);
+		clientSocket.close();
+		return Integer.valueOf(persistentPort);
+	}
+	
 	public static void main(String argv[]) throws Exception {
+		int port = getPort();
+		
 		String sentence;
 		String modifiedSentence;
 		
 		while (true) {
 			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-			Socket clientSocket = new Socket("localhost", 6789);
+			Socket clientSocket = new Socket("localhost", port);
 			
 			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 			
