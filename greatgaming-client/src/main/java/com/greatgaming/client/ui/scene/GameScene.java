b583ede1c15afb36aa6465de1dbed3c496ac364d
slone1632 <slone1632@gmail.com>
2018-11-25 14:16:19 -0500

    adding text wrapping to the list view for long messages

--- greatgaming-client/src/main/java/com/greatgaming/client/ui/scene/GameScene.java
+++ greatgaming-client/src/main/java/com/greatgaming/client/ui/scene/GameScene.java
@@ -8,11 +8,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
 
 public class GameScene {
     private final AggregateGameState aggregateGameState;
@@ -43,6 +46,28 @@ public class GameScene {
         chatListView = new ListView<>();
         chat = FXCollections.observableArrayList ();
         chatListView.setItems(chat);
+        chatListView.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                return new ListCell<String>() {
+                    {
+                        prefWidthProperty().bind(chatListView.widthProperty().subtract(20)); // 1
+                        setMaxWidth(Control.USE_PREF_SIZE); //2
+                    }
+
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        if (item != null && !empty) {
+                            this.setWrapText(true); // 3
+                            setText(item);
+                        } else {
+                            setText(null);
+                        }
+                    }
+
+                };
+            }
+        });
 
         messageText.setOnKeyPressed(new EventHandler<KeyEvent>()
         {
